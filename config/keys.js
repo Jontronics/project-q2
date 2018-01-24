59a896567a299484a863b48122cafca0af08ae44
Jonathan Aqauarone <J.aquarone@gmail.com>
2018-01-23 19:35:00 -0500

    something wrong with the token

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  googleClientID: '894819683893-ltchrch91nja66ilh3i64qhldpd6rvd9.apps.googleusercontent.com',
+  googleClientSecret: 'G6pF1BE6pcCZsfo5HJP9Xe8x' 
+};
+
--- index.js
+++ index.js
@@ -1,13 +1,40 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys.js');
 const app = express();
-//message commit -m "intial commit"
 
-app.get('/', (req, res) => {
-  
-  res.send({ hi: 'there for real?' });
-  
-  
-});
+passport.use(new GoogleStrategy({
+      clientID: keys.googleClientID, 
+      clientSecrete: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback'
+    }, (accessToken, refreshToken, profile, done) => {
+      console.log('<-------------------------accessToken',accessToken);
+      console.log('<-------------------------refreshToken',refreshToken);
+      console.log('<-------------------------profile',profile);
+
+    }
+  )
+);
+
+// route handle
+
+app.get('/auth/google', passport.authenticate('google', {
+  scope: ['profile', 'email']
+ })
+);
+
+// route handler two 
+
+app.get('/auth/google/callback', passport.authenticate('google'));
+
+
+// app.get('/', (req, res) => {
+//   res.send({ hi: 'there for real?' });
+// });  test route
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
+
+// 
+

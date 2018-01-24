1f295ad826e28385767c7721f7be383c8b1e5087
Jonathan Aqauarone <J.aquarone@gmail.com>
2018-01-23 20:05:16 -0500

    yay fixed secrete to secret buggggg

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 
 passport.use(new GoogleStrategy({
       clientID: keys.googleClientID, 
-      clientSecrete: keys.googleClientSecret,
+      clientSecret: keys.googleClientSecret,
       callbackURL: '/auth/google/callback'
     }, (accessToken, refreshToken, profile, done) => {
       console.log('<-------------------------accessToken',accessToken);
